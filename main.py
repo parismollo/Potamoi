505880f6432570b22b742bf7ae7b71b675857c51
Paris Mollo <parismollochristondis@gmail.com>
2020-12-12 19:42:53 +0100

    refactor(data validation): change validade data function - returns list of valid files instead of boolean

--- main.py
+++ main.py
@@ -3,9 +3,7 @@ from postgres.fetch_sample import get_sample
 from postgres.delete_data import reset_all_tables, delete_all_csv
 from postgres.download_data import download_all_tables
 from typing import List
-
-def validate_csv() -> bool:
-    return True
+from postgres.validate_data import validate_data
 
 def main():
     tables: List[str] = ["rainfall", "soil_moisture", "river_stage"]
@@ -14,9 +12,11 @@ def main():
     start_data_gen(target)
     download_all_tables(tables)
     reset_all_tables(tables)
-    # if validate_csv():
-    #     # send_data()
-    #     delete_all_csv(paths)
+
+    valid_files = validate_data()
+    if len(valid_files) != 0:
+        # send_data(valid_files)
+        delete_all_csv(paths)
 
 if __name__ == '__main__':
     main()
---
+++ postgres/validate_data.py
@@ -0,0 +1,38 @@
+import os
+from os import listdir
+from os.path import join
+import pandas as pd
+
+# TODO: filter data we want
+
+# def filter(dataframe):
+#     return dataframe.iloc[["id", "value"]]
+
+
+def check_target_cols(target, path):
+    '''
+    Check if all minimal target columns are in the dataframe.
+    '''
+    is_valid = True
+    for col in target:
+        is_valid = is_valid and (col in pd.read_csv(path).columns)
+    return is_valid
+
+
+def validate_data(max_size=500, data_dir="data/", accepted_file_formats = (".json", ".csv"), target=["value", "id"]):
+    valid_files = []
+    try:
+        directory_path = os.listdir(data_dir)
+        for filename in directory_path:
+            path = os.path.join(data_dir, filename)
+            if filename.endswith(accepted_file_formats) and os.path.getsize(path) <= max_size:
+                if check_target_cols(target, path):
+                    valid_files.append(path)
+                else:
+                    print(f"File {path} not valid")
+            else:
+                print(f"File {path} not valid")
+    except Exception as e:
+        print(e)
+
+    return valid_files

37695fb4acab5a7f486e3ba4dcb05b819da30701
gary <gary@localhost.localdomain>
2021-01-24 16:16:03 +0100

    [INIT] App Mode Datasets

--- user_interface/home.py
+++ user_interface/home.py
@@ -1,8 +1,24 @@
 import streamlit as st
+import pandas as pd
+from user_interface.quality_control import download_link
 
+def display_df(df,header,col):
+    col.header(header)
+    col.write(df)
+    tmp_download_link3 = download_link(df ,'YOUR_df.csv' ,'Download')
+    col.markdown(tmp_download_link3, unsafe_allow_html=True)
+    
 
 def home():
-    st.title("Datasets")
+    df = pd.read_csv('user_interface/submissionRFC_nosplit.csv')
+    st.title("How it works ?")
+    st.subheader('The Idea :bulb:')
+    st.write("fbjezkbkfezkfzekze")
+    col1, col2, col3 = st.beta_columns(3)
+    
+    display_df(df,"DF1",col1)
+    display_df(df,"DF2",col2)
+    display_df(df,"DF3",col3)
     # faire la page comme dans le dessin
     # query datasets
     # pour chaque dataset:
